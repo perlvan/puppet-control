3e2d0531181c5c113c123252b925e846e194af08
perlvan <pvanu_reddy@yahoo.com>
2017-11-03 13:04:57 +0000

    test node def

--- manifests/site.pp
+++ manifests/site.pp
@@ -25,6 +25,14 @@ File { backup => false }
 # will be included in every node's catalog, *in addition* to any classes
 # specified in the console for that node.
 
+node 'perlvan2.mylabserver' {
+   notify { 'THis matches a node name !!': }
+}
+
+node /^perlvan2/ {
+  notify { "This is a node definition using regex!!!": }
+}
+
 node default {
   # This is where you can declare classes for all nodes.
   # Example:
